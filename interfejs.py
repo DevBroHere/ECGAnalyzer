14973f2b040b82e10a822d43621cf4eae20e3ac1
Ppp-Komputer\Ppp <cezarybujak28@gmail.com>
2020-12-21 00:44:29 +0100

    Dodanie opcji konfiguracji okna (bez funkcjonalności) oraz paska toolbar

--- interfejs.py
+++ interfejs.py
@@ -15,17 +15,19 @@ from matplotlib.figure import Figure
 import matplotlib.animation as animation
 from matplotlib import style
 from matplotlib import pyplot as plt
-#Zrobiłem sobie coś
 import urllib
 import json
 
 import pandas as pd
 import numpy as np
 
+from PIL import Image, ImageTk
+
 LARGE_FONT = ("VERDANA", 12)
 NORM_FONT = ("VERDANA", 10)
 SMALL_FONT = ("VERDANA", 8)
 
+#Stałe używane do tymczasowego zapisu
 sampleType = 16
 sampleFrequency = None
 sampleResolution = None
@@ -34,137 +36,9 @@ sampleVoltage = None
 ##print(style.available) #Wyświetlanie możliwych styli
 style.use("bmh") #Wybranie danego stylu wyświetlania wykresów
 
-f = Figure() #Tworzymy obiekt Figure
-#figsize to wielkość tworzonej przestrzeni na wykres
-#(szerokość, wysokość) w calach, dpi to piksele na cal.
-filename = None
-a = f.add_subplot(111)#Możemy dodawać kolejne okna wedle upodobań
-#b = f.add_subplot(212)
+#Globalne dane ekg (bieżące)
 x_global = None
 y_global = None
-
-class EcgAnalizer(tk.Tk):
-
-    def __init__(self):
-        tk.Tk.__init__(self)
-        #tk.Tk.iconbitmap(self, default = "plik") Funkcja do ustawiania ikonki
-        tk.Tk.wm_title(self, "EcgAnalizer")
-        container = tk.Frame(self)
-
-        container.pack(side = "top", fill="both", expand = True)
-
-        container.grid_rowconfigure(0, weight = 1)
-        container.grid_columnconfigure(0, weight = 1)
-
-        #Tworzenie głównego paska menu
-        menubar = tk.Menu(container)
-        
-        #Tworzenie części paska "File"
-        filemenu = tk.Menu(menubar, tearoff = 0)
-        filemenu.add_command(label = "Open...",
-                             command = lambda: browseFiles())
-        filemenu.add_separator()
-        filemenu.add_command(label = "Save",
-                             command = lambda: popupmsg("Not supported yet"))
-        filemenu.add_command(label = "Save as...",
-                             command = lambda: popupmsg("Not supported yet"))
-        filemenu.add_separator()
-        filemenu.add_command(label = "Exit",
-                             command = exit)
-        menubar.add_cascade(label = "File", menu = filemenu)
-        
-        #Tworzenie części paska "Options"
-        optionsmenu = tk.Menu(menubar, tearoff = 0)
-        optionsmenu.add_command(label = "Sample",
-                                command = lambda: popupmsg("Not supported yet"))
-        optionsmenu.add_command(label = "Configure window",
-                                command = lambda: popupmsg("Not supported yet"))
-        menubar.add_cascade(label = "Options", menu = optionsmenu)
-        
-        #Tworzenie części paska "ECG analysis"
-        ecgmenu = tk.Menu(menubar, tearoff = 0)
-        ecgmenu.add_command(label = "Filter",
-                                command = lambda: popupmsg("Not supported yet"))
-        ecgmenu.add_command(label = "Analyze",
-                                command = lambda: popupmsg("Not supported yet"))
-        menubar.add_cascade(label = "ECG analysis", menu = ecgmenu)
-        
-        #Tworzenie części paska "Help"
-        helpmenu = tk.Menu(menubar, tearoff = 0)
-        helpmenu.add_command(label = "About program",
-                                command = lambda: popupmsg("Not supported yet"))
-        helpmenu.add_separator()
-        helpmenu.add_command(label = "Help",
-                                command = lambda: popupmsg("Not supported yet"))
-        menubar.add_cascade(label = "Help", menu = helpmenu)
-        
-        #Zatwierdzenie menu
-        tk.Tk.config(self, menu = menubar)
-
-        self.frames = {}
-
-        for F in (StartPage, ECG_page):
-
-            frame = F(container, self)
-
-            self.frames[F] = frame
-
-            frame.grid(row = 0, column = 0, sticky = "nsew")
-
-        self.show_frame(StartPage)
-
-    def show_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
-        
-    def refresh(self):
-        None
-
-    def plotData(self, x, y):
-        ECG_page.plotData(self, x, y)
-
-
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "BETA ECG analize application\n use a your own risk")
-        label.pack(pady = 10, padx = 10)
-
-        button1 = ttk.Button(self, text = "Agree",
-                            command = lambda: controller.show_frame(ECG_page))
-        button1.pack()
-
-        button2 = ttk.Button(self, text = "Disagree",
-                            command = quit)
-        button2.pack()
-
-
-
-class ECG_page(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "Graph page")
-        label.pack(pady = 10, padx = 10)      
-
-        canvas = FigureCanvasTkAgg(f, self)
-        canvas.draw()
-        canvas.get_tk_widget().pack(side = tk.TOP, fill=tk.BOTH, expand = True)
-
-        toolbar = NavigationToolbar2TkAgg(canvas, self)
-        toolbar.update()
-        canvas._tkcanvas.pack(side = tk.TOP, fill=tk.BOTH, expand = True)
-        
-    def plotData(self, x, y):
-        a.clear()
-        a.plot(x, y)
-        self.update()
-
-
-        
-app = EcgAnalizer()
-app.geometry("800x600")
 
 def popupmsg(msg):
     popup = tk.Tk()
@@ -244,15 +118,15 @@ def loadDataOptions():
         x_global = x
         y_global = y
 
-        app.plotData(x,y)
+        a.clear()
+        a.plot(x, y)
+        canvas.draw()
 
         window.destroy()
         
 
     applyButton = tk.Button(window, text = "Save and apply", command = lambda: apply())
-    #saveButton = tk.Button(window, text = "Save configuration", command = None)
     applyButton.grid(row=2, column = 1, sticky = "E", pady = 5)
-    #saveButton.grid(row=2, column = 0, sticky = "W", pady = 5)
 
     window.mainloop() # wywołanie pętli komunikatów
 
@@ -267,7 +141,135 @@ def browseFiles():
         if filename: 
             loadDataOptions()
 
-    
+def saveFiles(): 
+    a = filedialog.asksaveasfilename(filetypes=(("PNG Image", "*.png"),("All Files", "*.*")), 
+            defaultextension='.png', title="Window-2")
+    if a:
+        plt.savefig(a)
+
+window = tk.Tk()
+#tk.Tk.iconbitmap(self, default = "plik") Funkcja do ustawiania ikonki
+window.state('zoomed')
+window.title("EcgAnalizer")
+
+#Tworzenie głównego paska menu
+menubar = tk.Menu(window)
+        
+#Tworzenie części paska "File"
+filemenu = tk.Menu(menubar, tearoff = 0)
+filemenu.add_command(label = "Open...",
+                             command = lambda: browseFiles())
+filemenu.add_separator()
+filemenu.add_command(label = "Save",
+                             command = lambda: saveFiles())
+filemenu.add_command(label = "Save as...",
+                             command = lambda: popupmsg("Not supported yet"))
+filemenu.add_separator()
+filemenu.add_command(label = "Exit",
+                             command = exit)
+menubar.add_cascade(label = "File", menu = filemenu)
+        
+#Tworzenie części paska "Options"
+optionsmenu = tk.Menu(menubar, tearoff = 0)
+windowmenu = tk.Menu(optionsmenu, tearoff = 0)
+numberofwindowsMenu = tk.Menu(windowmenu, tearoff = 0)
+analyzesectionMenu = tk.Menu(windowmenu, tearoff = 0)
+
+optionsmenu.add_command(label = "Samples",
+                                command = lambda: popupmsg("Not supported yet"))
+
+numberofwindowsMenu.add_radiobutton(label = "1 window")
+numberofwindowsMenu.add_radiobutton(label = "2 windows")
+
+analyzesectionMenu.add_radiobutton(label = "right")
+analyzesectionMenu.add_radiobutton(label = "left")
+analyzesectionMenu.add_radiobutton(label = "top")
+analyzesectionMenu.add_radiobutton(label = "down")
 
 
-app.mainloop()
+menubar.add_cascade(label = "Options", menu = optionsmenu)
+optionsmenu.add_cascade(label = "Configure window", menu = windowmenu)
+windowmenu.add_cascade(label = "Windows", menu = numberofwindowsMenu)
+windowmenu.add_cascade(label = "Analyze section", menu = analyzesectionMenu)
+        
+#Tworzenie części paska "ECG analysis"
+ecgmenu = tk.Menu(menubar, tearoff = 0)
+ecgmenu.add_command(label = "Filter",
+                                command = lambda: popupmsg("Not supported yet"))
+ecgmenu.add_command(label = "Analyze",
+                                command = lambda: popupmsg("Not supported yet"))
+menubar.add_cascade(label = "ECG analysis", menu = ecgmenu)
+        
+#Tworzenie części paska "Help"
+helpmenu = tk.Menu(menubar, tearoff = 0)
+helpmenu.add_command(label = "About program",
+                                command = lambda: popupmsg("Not supported yet"))
+helpmenu.add_separator()
+helpmenu.add_command(label = "Help",
+                                command = lambda: popupmsg("Not supported yet"))
+menubar.add_cascade(label = "Help", menu = helpmenu)
+
+#Tworzenie sekcji toolbar
+toolbar = tk.Frame(window, bd=1, relief=RAISED)
+
+img = Image.open("save icon3.png")
+eimg = ImageTk.PhotoImage(img)
+
+exitButton = tk.Button(toolbar, image=eimg, relief=FLAT,
+                    command=quit)
+exitButton.image = eimg
+exitButton.pack(side=LEFT, padx=2, pady=2)
+
+toolbar.grid(row = 0, column = 0, columnspan = 2, sticky = "nsew")
+        
+#Zatwierdzenie menu
+window.config(menu = menubar)
+
+
+container = tk.Frame()
+container2 = tk.Frame()
+
+container.grid(row=1, column=0, sticky="nsew")
+container2.grid(row=1, column=1, sticky="nsew")
+#********************************
+#container.grid(row=0, column=0, sticky="nsew")
+#container2.grid(row=1, column=0, sticky="nsew")
+#********************************
+#container2.grid(row=0, column=0, sticky="nsew")
+#container.grid(row=0, column=1, sticky="nsew")
+
+window.grid_columnconfigure(0, weight=3)
+window.grid_columnconfigure(1, weight=1)
+window.grid_rowconfigure(0, weight=1)
+window.grid_rowconfigure(1, weight=1000)
+#********************************
+#window.grid_rowconfigure(0, weight = 3)
+#window.grid_rowconfigure(1, weight = 1)
+#window.grid_columnconfigure(0, weight = 1)
+#********************************
+#window.grid_columnconfigure(0, weight=1)
+#window.grid_columnconfigure(1, weight=3)
+#window.grid_rowconfigure(0, weight=1)
+
+var = StringVar()
+label = Label( container2, textvariable=var)
+
+var.set("ECG analysis")
+label.pack()
+
+f = Figure() #Tworzymy obiekt Figure
+#figsize to wielkość tworzonej przestrzeni na wykres
+#(szerokość, wysokość) w calach, dpi to piksele na cal.
+a = f.add_subplot(212)#Możemy dodawać kolejne okna wedle upodobań
+b = f.add_subplot(211)
+
+canvas = FigureCanvasTkAgg(f, master = container)
+canvas.draw()
+canvas.get_tk_widget().pack(side = tk.TOP, fill=tk.BOTH, expand = True)
+
+toolbar = NavigationToolbar2TkAgg(canvas, container)
+toolbar.update()
+canvas._tkcanvas.pack(side = tk.TOP, fill=tk.BOTH, expand = True)
+
+
+window.mainloop()
